8006eb1bbe50ee8a7362bf12cb98044bc063a096
Derek Kilgore <derek.kilgore@gmail.com>
2022-10-23 18:31:46 -0500

    models fixed

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,27 +1,30 @@
 const { Schema, Types } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
+const moment = require('moment');
 
-const ReactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: "Please enter your reaction",
-            maxLength: 280
-        },
-        username: {
-            type: String,
-            required: "You need a username!",
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal),
-        }
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId, 
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm:a')
     }
-);
+}, {
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
 
-module.exports = ReactionSchema;
+module.exports = reactionSchema;
